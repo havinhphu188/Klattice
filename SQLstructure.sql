da1b88c858e2a7d88ba239ac63ed5c34a6d8635f
Mark Spiers <mspiers405@gmail.com>
2019-07-15 11:13:48 +0100

    updated sql to include min length for roleName

--- SQLstructure.sql
+++ SQLstructure.sql
@@ -1,13 +1,16 @@
 DROP DATABASE IF EXISTS klattice;
-
 CREATE DATABASE klattice;
-
 USE klattice;
 
 CREATE TABLE Role (
-	roleID INT PRIMARY KEY auto_increment,
-    roleName varchar(60)
+	roleID INT unsigned,
+    roleName varchar(60),
+    PRIMARY KEY (`roleName`)
 );
+
+ALTER TABLE Role
+ADD CONSTRAINT roleNameLen
+CHECK (length(roleName) >= 4);
 
 INSERT INTO Role (roleName)
 VALUES ('role 1'), ('role 2'), ('role 3'), ('role 5');
