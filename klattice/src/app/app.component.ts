75ba2e3fe2319f9d97dd2d43f1e0be5d587fedf9
Matthew Leyburn <mattleyburn@hotmail.co.uk>
2019-07-15 14:38:42 +0100

    connect front and back end

--- klattice/src/app/app.component.ts
+++ klattice/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Role } from './classes/role';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +9,38 @@ import { Role } from './classes/role';
 })
 
 export class AppComponent {
+
   title = 'klattice';
-  roles = ROLES;
+
+  roles = this.http.get<Role[]>('/api/getroles');
+
+  constructor(private http: HttpClient) {
+    
+    console.log(this.roles);
+  }
+
+  
+  
+  
+  //title = 'klattice';
+  //roles = ROLES;
 }
 
-const ROLES: Role[] = [
-  {
-    roleID: 1,
-    roleName: 'Software Engineer'
-  },
-  {
-    roleID: 2,
-    roleName: 'Marketing'
-  },
-  {
-    roleID: 3,
-    roleName: 'CEO'
-  },
-  {
-    roleID: 4,
-    roleName: 'Water boy'
-  }
-];
+// const ROLES: Role[] = [
+//   {
+//     roleID: 1,
+//     roleName: 'Software Engineer'
+//   },
+//   {
+//     roleID: 2,
+//     roleName: 'Marketing'
+//   },
+//   {
+//     roleID: 3,
+//     roleName: 'CEO'
+//   },
+//   {
+//     roleID: 4,
+//     roleName: 'Water boy'
+//   }
+// ];
