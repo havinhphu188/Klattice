266b46aa2d53fd1ba897987b65f551175711f3e9
Broom94 <thomaswilliambroom@gmail.com>
2019-07-16 14:01:16 +0100

    user story for express modified

--- api/db.js
+++ api/db.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
 
 exports.getRoles = function (callback) {
     db.query(
-        "SELECT role_id, role_name, role_summary, role_sum_link, capability_id FROM role;",
+        "SELECT role_id, role_name, role_summary, role_sum_link, capability_id, band_id FROM role;",
         function (err, rows) {
             if (err, rows) {
                 callback(rows);
@@ -21,6 +21,17 @@ exports.getRoles = function (callback) {
 exports.getCapability = function(callback) {
     db.query(
         "SELECT capability_id, capability_name FROM capability;",
+        function (err, rows) {
+            if(err, rows) {
+                callback(rows);
+            }
+        }
+    );
+}
+
+exports.getBands = function(callback) {
+    db.query(
+        "SELECT band_id, band_name, level, competency FROM band",
         function (err, rows) {
             if(err, rows) {
                 callback(rows);
