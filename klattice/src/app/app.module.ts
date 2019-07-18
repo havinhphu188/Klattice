57d33cdb82232dace7d7177730e900c017f5fafb
Matthew Fox <matthew.fox@kainos.com>
2019-07-18 09:46:31 +0100

    navbar

--- klattice/src/app/app.module.ts
+++ klattice/src/app/app.module.ts
@@ -7,12 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RoleModalComponent } from './components/role-modal/role-modal.component';
 import { SortByPipe } from './sort-by.pipe';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RoleModalComponent,
-    SortByPipe
+    SortByPipe,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
