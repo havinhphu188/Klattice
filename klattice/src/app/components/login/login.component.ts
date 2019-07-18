1c4f90c28bbeb413250c703c3f2d7f6d865b91bc
Ryan McDonald <ryanmc@Precision.local>
2019-07-18 11:01:59 +0100

    updated login code

--- klattice/src/app/components/login/login.component.ts
+++ klattice/src/app/components/login/login.component.ts
@@ -30,73 +30,42 @@ export class LoginComponent {
 
   
 }
-
+  signInStatus = 'f';
   getHeaders() {
       return (this.headers = {
       "Content-Type": "application/json"
       });
     }
-    
-    compareHash(textPassword, salt, hashedpassword){
-      var result = bcrypt.compareSync(textPassword + salt, hashedpassword);
-      return result;
-    }
 
-    createHash(textPassword, salt, saltInt){
-      var hash = bcrypt.hashSync(textPassword + salt, saltInt);
-      return hash;
-    }
-    
  onClickSubmit(username, password) {
 
   let formData: FormData = new FormData(); 
-  var username= this.angForm.controls.username.value;
-  var password = this.angForm.controls.password.value;
-
-  var salt = bcrypt.genSaltSync(10);
-  
-  var hash = this.createHash(password, "EH189020", salt);
-
-   //TESTCASE  -- positive
-  var isValidLogin = this.compareHash("adminPass", "EH189020", "$2a$10$TRBNEkxNFP0bI49FooWXHuP80gs63w38X4lh2lvcuQfHOCNgWcbbK");
- //TESTCASE -- negative
-  var notValidLogin = this.compareHash("adminPass", "EH189020", "$2a$10$TRBNEkxNFP0bI49FooWXHuP80gs63w38X4lh2lvcuQfH");
-  console.log("isValidLogin TEST RS: true :" + isValidLogin);
-  console.log("notValidLogin TEst RS: false :" + notValidLogin);
-
-   console.log(hash);
-
-   formData.append('username', username); 
-   formData.append('password', hash); 
-
 
    alert('Your username is : '+ username);
-   let params = {"username": username, "password":hash};
+   let params = {"username": username, "password":password};
    
    this.headers = {
     "Content-Type": "application/json"
     }
     this.http.post('/api/userdetails', {params:params}, this.headers)
     .subscribe(response=>{
-
-      console.log('repsonse ', response);
-
       var rsp = JSON.stringify(response);
       rsp = rsp.substring(11,12);
       
       if(rsp == 'a')
       {
-        alert('whats happening admin  kid');
+        alert('Admin signed in');
       }
       else if(rsp == 'e')
       {
-        alert("whats happening employee kid");
+        alert("Employee signed in");
       }
       else if(rsp == 'f')
       {
         alert("Failed");
         window.location.reload();
       }
+      this.signInStatus = rsp;
     })
     
   }
